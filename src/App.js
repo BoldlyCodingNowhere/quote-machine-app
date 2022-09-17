7c432e699a1411c461b0062d1e48eb21ec639321
BoldlyCodingNowhere <imnotcoding101@gmail.com>
2022-09-17 04:28:24 +0100

    finish styling app, adding color component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import useGenerateRandomColor from "./useGenerateRandomColor";
 import { FaTwitterSquare, FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
 
 function App() {
   const [quoteInfo, setQuoteInfo] = useState({});
+  const { color, generateColor } = useGenerateRandomColor();
+
+  const styles = {
+    back: {
+      backgroundColor: `#${color}`,
+    },
+    textCol: {
+      color: `#${color}`,
+    },
+  };
 
   useEffect(() => {
     getQuote();
   }, []);
 
   const getQuote = () => {
+    generateColor();
     fetch("https://api.quotable.io/random")
       .then((response) => response.json())
       .then((data) => {
@@ -24,30 +36,41 @@ function App() {
   };
 
   return (
-    <div className="app-container">
-      <div id="quote-box">
-        <p id="text">
-          <i className="quote-left">
-            <FaQuoteLeft />
-          </i>
-          {quoteInfo.text}
-          <i className="quote-right">
-            <FaQuoteRight />
-          </i>
-        </p>
-        <p id="author">{quoteInfo.author}</p>
-        <div className="buttons">
-          <button id="new-quote" onClick={getQuote}>
-            New Quote
-          </button>
-          <a
-            id="tweet-quote"
-            className="twitter-share-button"
-            href="https://twitter.com/intent/tweet"
-            target="_blank"
-          >
-            <FaTwitterSquare />
-          </a>
+    <div className="app-component" style={styles.back}>
+      <div className="app-container">
+        <div id="quote-box">
+          <p id="text" style={styles.textCol}>
+            <i className="quote-left">
+              <FaQuoteLeft />
+            </i>
+            {quoteInfo.text}
+            <i className="quote-right">
+              <FaQuoteRight />
+            </i>
+          </p>
+          <p id="author">{quoteInfo.author}</p>
+          <div className="buttons">
+            <a
+              id="tweet-quote"
+              className="twitter-share-button"
+              href="https://twitter.com/intent/tweet"
+              target="_blank"
+            >
+              <FaTwitterSquare
+                style={styles.textCol}
+                className="twitter-button"
+              />
+            </a>
+            <button
+              type="button"
+              className="button"
+              id="new-quote"
+              style={styles.back}
+              onClick={getQuote}
+            >
+              New Quote
+            </button>
+          </div>
         </div>
       </div>
     </div>
